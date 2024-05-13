a4a8b8b6423a0d605a9d6602d361b2aa451ea16c
SmartSystem <mrozimatov9@gmail.com>
2024-05-13 14:04:53 +0500

    added KeyEvent

--- app.js
+++ app.js
@@ -26,3 +26,26 @@ document.addEventListener("keydown", (e) => {
     addHidden();
   }
 });
+
+// KeyEvent
+
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+
+        <div class="key">
+            ${e.key == " " ? "Space" : e.key}
+            <small>Event.key</small>
+        </div>
+        <div class="key">
+            ${e.keyCode}
+            <small>Event.keyCode</small>
+        </div>
+        <div class="key">
+            ${e.code}
+           <small>Event.code</small>
+        </div>
+
+    `;
+});
